634b5a29624e4b4d27a4188b7c283f0e249a76db
맥찡 <rkdeorb0124@naver.com>
2023-06-16 15:57:22 +0900

    기본 설정 및 jsp파일 옮기기

---
+++ src/main/java/com/springlec/base/config/DataAccessConfig.java
@@ -0,0 +1,37 @@
+package com.springlec.base.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan(basePackages = "com.springlec.base.dao") // 패키지 이름이변경되면 바꿔줘야됨
+public class DataAccessConfig {
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setMapperLocations(
+				new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml") // 경로위치 달라지면 바꿔줘여됨
+				);
+		return sessionFactory.getObject();
+	} 
+	
+	// sql문장을 나눠주는 작업
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+		
+	}
+	
+	
+	
+	
+}
