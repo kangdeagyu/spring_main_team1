e3a9d6e2c3c69ef56e9098315121d99ff9ef2275
ms990310 <130430405+ms990310@users.noreply.github.com>
2023-06-19 12:18:34 +0900

    정정

--- src/main/java/com/springlec/base/service/Kms_Forum_ServiceImpl.java
+++ src/main/java/com/springlec/base/service/Kms_Forum_ServiceImpl.java
@@ -9,10 +9,10 @@ import com.springlec.base.dao.Kms_Forum_Dao;
 import com.springlec.base.model.Kms_Forum_Dto;
 
 @Service
-public class Kms_Forum_ServiceImpl implements Kms_Forum_Dao {
+public class Kms_Forum_ServiceImpl implements Kms_Forum_Service {
 	
 	@Autowired
-	Kms_Forum_Service dao;
+	Kms_Forum_Dao dao;
 
 	@Override
 	public List<Kms_Forum_Dto> list(int ftype) throws Exception {
