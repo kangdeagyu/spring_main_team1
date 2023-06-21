9079c027fcb7db40a029798510c6d87746903776
kkglpp <kkglpp@gmail.com>
2023-06-21 14:22:34 +0900

    123
    123

--- target/classes/static/js/admin_kkg_userlist.js
+++ target/classes/static/js/admin_kkg_userlist.js
@@ -11,8 +11,12 @@ function SingleBarChart(context, xlabels, y01, ydatas_01) {
       {
         label: y01,
         fill: false,
-        backgroundColor: ['#B4D9FF', '#007AFF', '#5856D6', '#34C759', '#FF2D55', '#FF9500', '#FFCC00', '#8E8E93', '#FF3B30', '#C69C6D'],
-        borderColor: ['#B4D9FF', '#007AFF', '#5856D6', '#34C759', '#FF2D55', '#FF9500', '#FFCC00', '#8E8E93', '#FF3B30', '#C69C6D'],
+        backgroundColor: [
+  '#B4D9FF', '#007AFF', '#5856D6', '#34C759', '#FF2D55', '#FF9500', '#FFCC00', '#8E8E93', '#FF3B30', '#C69C6D'
+],
+        borderColor: [
+  '#B4D9FF', '#007AFF', '#5856D6', '#34C759', '#FF2D55', '#FF9500', '#FFCC00', '#8E8E93', '#FF3B30', '#C69C6D'
+],
         data: ydatas_01,
       },
     ],
