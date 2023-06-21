6889c5f66c03e832e7e2ca9ccf41e19bb79526c9
김종진 <rlawhdwls55@naver.com>
2023-06-21 12:21:00 +0900

    바로구매 완료, db 재고량 삭제 완료

--- src/main/java/com/springlec/base/service/KJJPurchaseNowDaoServiceImpl.java
+++ src/main/java/com/springlec/base/service/KJJPurchaseNowDaoServiceImpl.java
@@ -11,21 +11,20 @@ public class KJJPurchaseNowDaoServiceImpl implements KJJPurchaseNowDaoService {
 	@Autowired
 	KJJPurchaseNowDao dao;
 
+
 	@Override
-	public void purchase(String cid, int pid, int oqty, int oprice, String opostnum, String oaddress1, String oaddress2,
-			String omemo, String opaytype) throws Exception {
+	public void PurchaseNowDao(String cid, int pid, int qty, int price, String postnum, String address1,
+			String address2, String memo, String payment) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println(cid);
-		System.out.println(pid);
-		System.out.println(oqty);
-		System.out.println(oprice);
-		System.out.println(opostnum);
-		System.out.println(oaddress1);
-		System.out.println(oaddress2);
-		System.out.println(omemo);
-		System.out.println(opaytype);
-		dao.purchase(cid, pid, oqty, oprice, opostnum, oaddress1, oaddress2, omemo, opaytype);
+		dao.PurchaseNowDao(cid, pid, qty, price, postnum, address1, address2, memo, payment);
+		
 	}
-	
+
+
+	@Override
+	public void prodcutQtyModify(int pid, int qty) throws Exception {
+		// TODO Auto-generated method stub
+		dao.prodcutQtyModify(pid, qty);
+	}
 
 }
